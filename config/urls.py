4248d1fe070d028eb3dd6a59577c5d86296f91ae
AbdelrahmanOuf <149276586+AbdelrahmanOuf@users.noreply.github.com>
2026-09-10 17:09:04 +0300

    feat: serve uploaded media in production

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,14 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve as media_serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("celebration.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.SERVE_MEDIA_FILES:
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", media_serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
