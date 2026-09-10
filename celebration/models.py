a0d568e117a31edce6c5427717d8354bb519d7a9
AbdelrahmanOuf <149276586+AbdelrahmanOuf@users.noreply.github.com>
2026-09-10 16:49:36 +0300

    refactor: rebrand couple name to Abdelrahman and Omnia

--- celebration/models.py
+++ celebration/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class EventSite(models.Model):
-    couple_names = models.CharField(max_length=120, default="Khaled & Huda")
+    couple_names = models.CharField(max_length=120, default="Abdelrahman & Omnia")
     eyebrow = models.CharField(max_length=120, default="Together, always")
     hero_title = models.CharField(max_length=180, default="Our Forever Begins Here")
     hero_subtitle = models.CharField(
